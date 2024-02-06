0386cd6eb5b86d68d0a08782296d6cfdfc77426c
James6345 <rossjrjames06@gmail.com>
2024-02-05 19:45:08 -0500

    Movement
    Movement Script

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed;
+    float speedX, speedY;
+    Rigidbody2D rb;
+    
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+   
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * movementSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * movementSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
