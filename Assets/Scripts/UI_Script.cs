6218ba35a3088a7c7a3c1bf2320f962154c44462
janeday1 <jane.d.cats@gmail.com>
2024-02-27 19:24:49 -0500

    How to Play screen and combining UI scripts
    Created new how to play canvas prefab that is seen before the game starts.
    Also made some of the UI Script stuff less confusing. Put End Menu script and its functions into UI Script. Tried to explain how some of it works with comments.
    Lastly, I made it so there is only one UI Script in the editor, it's on GameManager. Updated all of the Button OnClick() functions in the editor to match that.

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -4,51 +4,89 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEditor;
+using UnityEngine.SceneManagement;
+
 
 public class UI_Script : MonoBehaviour
 {
-    public GameObject canvas;
-    public Rigidbody2D player1;
-    public Movement movementScript;
+    //use these to switch canvas in scene
+    public CanvasGroup gameCanvas;
+    public CanvasGroup howToPlay;
 
-    public TMP_Text p1;  //text that displays scores
+    //text that displays scores
+    public TMP_Text p1;  
     public TMP_Text p2;
 
-    //public GameObject p1_heart_1;  //no longer necessary but keeping just in case
-    //public GameObject p1_heart_2;
-    //public GameObject p1_heart_3;
-    //public GameObject p2_heart_1;
-    //public GameObject p2_heart_2;
-    //public GameObject p2_heart_3;
+    //ui turned off at endgame
+    public GameObject p1OldTitle;
+    public GameObject p1Before;
+    public GameObject p2OldTitle;
+    public GameObject p2Before;
 
-    protected bool p1win = false;
-    protected bool p2win = false;
+    //ui turned on at endgame
+    public GameObject p1NewTitle;
+    public GameObject p1End;
+    public TMP_Text p1FinalScore;
+    public GameObject p2NewTitle;
+    public GameObject p2End;
+    public TMP_Text p2FinalScore;
 
-    protected int p1_score = 0;
-    protected int p2_score = 0;
-    int scoreCap = 3;  //change to score that makes sense
+    public GameObject win;
+    public TMP_Text winText;
+    public GameObject RestartButton;
+    public GameObject EndButton;
+
+    //players stop moving at endgame
+    Rigidbody2D player1;
+    Movement movementp1;
+    Rigidbody2D player2;
+    Movement movementp2;
+
+    bool p1win = false;
+    bool p2win = false;
+
+    //score
+    int p1_score = 0;
+    int p2_score = 0;
+    int scoreCap = 5;  //change to score that makes sense
 
 
     void Start()
     {
         p1.text = "0";
         p2.text = "0";
+
+        //display howToPlay canvas
+        howToPlay.alpha = 1;
+        gameCanvas.alpha = 0;
     }
 
     void Update()
     {
-        DisplayScore();
-
         if (p1win || p2win)
         {
-            //movementScript.enabled = false;  //inputs will no longer move the frog when game is over
-            player1.velocity = Vector3.zero;  //freeze frog
-            canvas.SendMessage("EndGame");
+            //stop frogs from moving after game is over
+            FindPlayers();
+            movementp1.enabled = false;  
+            player1.velocity = Vector3.zero;
+            if (player2 != null)
+            {
+                movementp2.enabled = false;  
+                player2.velocity = Vector3.zero;
+            }
+
+            //change UI
+            EndGame();
+        }
+        else
+        {
+            DisplayScore();
         }
     }
 
     void DisplayScore()
     {
+        //BugManager updates score text directly
         p1_score = int.Parse(p1.text);
         p2_score = int.Parse(p2.text);
 
@@ -62,40 +100,106 @@ public class UI_Script : MonoBehaviour
         }
     }
 
+    public void ChangeCanvas()
+    {
+        //Happens when Ready! button on HowToPlay canvas is pushed
 
-    //public void P1Health()
-    //{
-    //    if (p1_heart_1.activeSelf)
-    //    {
-    //        p1_heart_1.SetActive(false);
-    //    }
-    //    else if (p1_heart_2.activeSelf)
-    //    {
-    //        p1_heart_2.SetActive(false);
-    //    }
-    //    else if (p1_heart_3.activeSelf)
-    //    {
-    //        p1_heart_3.SetActive(false);
-    //        p1win = true;
-    //    }
-    //    else { }
-    //}
+        //display main game canvas
+        gameCanvas.alpha = 1;
+        howToPlay.alpha = 0;
 
-    //public void P2Health()
-    //{
-    //    if (p2_heart_1.activeSelf)
-    //    {
-    //        p2_heart_1.SetActive(false);
-    //    }
-    //    else if (p2_heart_2.activeSelf)
-    //    {
-    //        p2_heart_2.SetActive(false);
-    //    }
-    //    else if (p2_heart_3.activeSelf)
-    //    {
-    //        p2_heart_3.SetActive(false);
-    //        p2win = true;
-    //    }
-    //    else { }
-    //}
+        //don't display endgame ui
+        p1NewTitle.SetActive(false);
+        p1End.SetActive(false);
+        p2NewTitle.SetActive(false);
+        p2End.SetActive(false);
+        win.SetActive(false);
+        RestartButton.SetActive(false);
+        EndButton.SetActive(false);
+    }
+
+    public void Restart()
+    {
+        //Happens when try again is pushed
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void BackToStartMenu()
+    {
+        //Happens when Back to Start is pushed
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void EndGame()
+    {
+        //final score is calculated
+        p1FinalScore.text = p1_score.ToString();
+        p2FinalScore.text = p2_score.ToString();
+
+        //turn off old score ui
+        p1OldTitle.SetActive(false);
+        p2OldTitle.SetActive(false);
+        p1Before.SetActive(false);
+        p2Before.SetActive(false);
+
+        StartCoroutine(EndDisplayTimer());
+    }
+
+    IEnumerator EndDisplayTimer()
+    {
+        //pause the game somehow without pausing this coroutine- Time.timeScale = 0 paused this method too;
+
+        p1NewTitle.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        p1End.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        p2NewTitle.SetActive(true);
+
+        yield return new WaitForSeconds(1);
+
+        p2End.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+
+        win.SetActive(true);
+
+        //determine who won
+        if (p1win && p2win)
+        {
+            winText.text = "DRAW!";
+        }
+        else if (p1win)
+        {
+            winText.text = "PLAYER 1 WINS!";
+        }
+        else
+        {
+            winText.text = "PLAYER 2 WINS!";
+        }
+
+        yield return new WaitForSeconds(1);
+
+        //display buttons
+        RestartButton.SetActive(true);
+        EndButton.SetActive(true);
+    }
+
+    void FindPlayers()
+    {
+        GameObject frog1 = GameObject.Find("Player");
+        player1 = frog1.GetComponent<Rigidbody2D>();
+        movementp1 = frog1.GetComponent<Movement>();
+
+        GameObject frog2 = GameObject.Find("Player2");
+        if (player2 != null)
+        {
+            player2 = frog2.GetComponent<Rigidbody2D>();
+            movementp2 = frog2.GetComponent<Movement>();
+        }
+    }
+
 }
